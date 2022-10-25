58337c00eef3539f9b7169eec13450f8f4314264
Harry <68451409+lujiazho@users.noreply.github.com>
2022-10-25 11:22:04 -0700

    Update index.js

--- postgres-backendMVC-demo/app/api/src/index.js
+++ postgres-backendMVC-demo/app/api/src/index.js
@@ -2,7 +2,10 @@ import app from "./app.js";
 import { sequelize } from "./database/database.js";
 
 async function main() {
+  // {force: false} means do not drop table if exists else create new one (database must exist first)
+  console.log("connect database and sync all tables");
   await sequelize.sync({force: false});
+  console.log("Trying to listen to port");
   app.listen(4000);
   console.log("Server on port 4000");
 }
